0ac68c030a264c33e671cd8323b2c24e555ced14
hidelya <laura.delfairiere@gmail.com>
2026-06-04 16:47:49 +0200

    maj 04-06-26

---
+++ LaboHeroesVsMonster/LaboHeroesVsMonster/Class/ZoneJeu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboHeroesVsMonster.Class
+{
+    public class ZoneJeu
+    {
+        string[,] tab = new string[15,15] ;
+
+        public void AfficherTableau() 
+        { 
+            for (int x = 0; x < tab.GetLength(0); x++)
+            {
+                for (int y = 0; y < tab.GetLength(1); y++)
+                {
+                    
+                    Console.Write(tab[x,y] + "|" + "__");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        
+    }
+}
