bd0a4c873f9a1f79d61415bf7fa3faba6f3f9f2a
hidelya <laura.delfairiere@gmail.com>
2026-06-05 10:57:47 +0200

    05-06-10:56

---
+++ LaboHeroesVsMonster/Class/Hero.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LaboHeroesVsMonster.Class;
+
+namespace LaboHeroesVsMonster.Class
+{
+    public class Hero : Perso
+    {
+          
+
+        public Hero() : base()
+        {
+           
+        }
+
+       
+       
+
+
+
+    }
+}
---
+++ LaboHeroesVsMonster/Class/Orque.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboHeroesVsMonster.Class
+{
+    public class Orque : Monster
+    {
+        public Orque()
+        {
+            Or = NombreOr();
+            forc += 1;
+        }
+    }
+}
